2e14cea5b704fa07f700dfb666a8e7665081df1a
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-11-30 10:30:27 +0100

    add if __name__ == "main":

--- src/main.py
+++ src/main.py
@@ -9,35 +9,34 @@ import copy
 
 import simulation
 
-random.seed(time.time())
-
-N_runs = 10000
-
-#Without treatment :
-simulation1 = simulation.run(nb_runs=N_runs, 
-                  N=2000, 
-                  prop_cancer= 0.1, 
-                  treatment=False, 
-                  verbose=True,
-                  weighted_reproduction = True,
-                  weighted_apoptosis = True,
-                  init_clones=10)
-
-
-#With treatment :
-simulation2 = simulation.run(nb_runs=N_runs, 
-                   N=2000, 
-                   prop_cancer=0.1, 
-                   omega=0.25, 
-                   alpha=0.5, T=10, 
-                   treatment=True, 
-                   nb_treatments=2, 
-                   verbose=True,
-                   weighted_reproduction = True,
-                   weighted_apoptosis = True,
-                   init_clones=10)
-
-
 if __name__ == "__main__" :
-	
-	print("main")
+
+    random.seed(time.time())
+
+    N_runs = 10000
+
+    #Without treatment :
+    simulation1 = simulation.run(nb_runs=N_runs, 
+                      N=2000, 
+                      prop_cancer= 0.1, 
+                      treatment=False, 
+                      verbose=True,
+                      weighted_reproduction = True,
+                      weighted_apoptosis = True,
+                      init_clones=10)
+
+
+    #With treatment :
+    simulation2 = simulation.run(nb_runs=N_runs, 
+                       N=2000, 
+                       prop_cancer=0.1, 
+                       omega=0.25, 
+                       alpha=0.5, T=10, 
+                       treatment=True, 
+                       nb_treatments=2, 
+                       verbose=True,
+                       weighted_reproduction = True,
+                       weighted_apoptosis = True,
+                       init_clones=10)
+
+

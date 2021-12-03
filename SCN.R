1d53d880fe321e293ec8fbef0aa8749072b8a431
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-12-03 22:23:39 +0100

    Revert "modified mu parameter for Susceptible cells"
    This reverts commit 4fc4362e9483e2110eaae84defd486fcee7899d2.

--- SCN.R
+++ SCN.R
@@ -4,7 +4,7 @@ rm(list = ls())
 SCN <- function(t, state, parameters)
 {
   with(as.list(c(state, parameters)), {
-    dS <- S * (beta1 * N/(N+K) - gamma)
+    dS <- S * (beta1 * N/(N+K) - mu - gamma)
     dC <- C * (beta2 * N/(N+K) - delta - tau) + mu * S
     dN <- - N/(N+K) * (beta1 * S + beta2 * C)
     list(c(dS, dC, dN))
@@ -12,7 +12,7 @@ SCN <- function(t, state, parameters)
 }
   
 ####### Model SCN ####### 
-params = c(mu = 0.02, beta1 = 0.04, beta2 = 0.07, gamma = 0.02, delta = 0.04, tau = 0.01, K = 1e2)
+params = c(mu = 0.01, beta1 = 0.05, beta2 = 0.07, gamma = 0.02, delta = 0.025, tau = 0.01, K = 1e2)
 init = c(S = 100, C = 25 , N = 1e3)
 times <- seq(0, 200, by=0.1)
 

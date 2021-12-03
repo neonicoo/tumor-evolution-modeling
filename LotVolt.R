a1b39fff0e4e4b0bfe92323514bbc611568dbb6d
neonicoo <nicolas.mendiboure@insa-lyon.fr>
2021-12-03 20:16:59 +0100

    Minor changes

--- LotVolt.R
+++ LotVolt.R
@@ -5,7 +5,7 @@ Lotka.Volterra <- function(t, state, parameters)
 {
   with(as.list(c(state, parameters)), {
     dX <- X * (alpha - beta * Y)
-    dY <-Y * (delta * beta * X - gamma)
+    dY <- Y * (delta * beta * X - gamma)
     list(c(dX, dY))
   })
 }
@@ -19,7 +19,7 @@ delta <- 0.5
 
 params = c(alpha, beta, gamma, delta)
 
-init = c(X = 10, Y = 5)
+init = c(X = 10, Y = 2)
 time <- seq(0, 100, by=0.1)
 
 out <- as.data.frame(ode(y = init, times = time, func = Lotka.Volterra, parms = params))
@@ -27,7 +27,7 @@ out <- as.data.frame(ode(y = init, times = time, func = Lotka.Volterra, parms = 
 S <- out$X
 C <- out$Y
 
-plot(time, S, type = "l", las=1, col = "blue")
+plot(time, S, type = "l", las=1, col = "blue", ylim = c(0, max(S, C)))
 lines(time, C, type = "l", col="red")
 
 legend("topright", legend=c("Normal cells", "Cancerous cells"),
--- SCN.R
+++ SCN.R
@@ -4,17 +4,17 @@ rm(list = ls())
 SCN <- function(t, state, parameters)
 {
   with(as.list(c(state, parameters)), {
-    dS <- -mu*S - gamma*S + beta1*(N/(N+K))*S
-    dC <- mu*S - delta*C + beta2*(N/(N+K))*C - tau*C
-    dN <- - beta1* (N/(N+K))*S - beta2*(N/(N+K))*C
+    dS <- S * (beta1 * N/(N+K) - mu - gamma)
+    dC <- C * (beta2 * N/(N+K) - delta - tau) + mu * S
+    dN <- - N/(N+K) * (beta1 * S + beta2 * C)
     list(c(dS, dC, dN))
   })
 }
   
 ####### Model SCN ####### 
-params = c(mu = 0.002, beta1 = 0.001, beta2 = 0.002, gamma = 0.0005, delta = 0.001, tau = 0.001, K = 1e2)
-init = c(S = 2000,C = 200 ,N = 1e4)
-times <- seq(0, 2000, by=0.01)
+params = c(mu = 0.01, beta1 = 0.05, beta2 = 0.07, gamma = 0.02, delta = 0.025, tau = 0.01, K = 1e2)
+init = c(S = 100, C = 25 , N = 1e3)
+times <- seq(0, 200, by=0.1)
 
 out <- as.data.frame(ode(y = init, time = times, func = SCN, parms = params))
 
@@ -25,7 +25,8 @@ N = out$N
 
 plot(t,S, type = "l", las=1, col = "blue", ylim = c(0, max(S, C)))
 lines(t, C, type = "l", col="red")
+lines(t, N, type = "l", col="forestgreen")
 
-legend("topright", legend=c("S cells", "C cells"),
-       col=c("blue", "red"), lty = 1)
+legend("topright", legend=c("S cells", "C cells", "Nutrients"),
+       col=c("blue", "red", "forestgreen"), lty = 1)
 
